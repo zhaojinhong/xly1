6c0f28c8ebbf5a170d68e791b2aaac533659f40e
okh258 <root@>
2019-05-12 18:18:05 +0800

    逗比审题不明！

--- lesson01/yixiang/exercise03-sum.py
+++ lesson01/yixiang/exercise03-sum.py
@@ -4,7 +4,7 @@
 sum = 0
 for i in range(5):
     sum += int(input("Please input number: "))
-print(sum)
+print("sum: ", sum)
 
 
 # 需求：
@@ -12,18 +12,20 @@ print(sum)
 sum = 0
 for i in range(100):
     sum += i + 1
-print(sum)
+print("1+...+100 =", sum)
 
 
 # 需求：
 #     输入n个数，知道输入0结束
 max = 0
+sum = 0
 while True:
     in1 = int(input("Please input number: "))
     if in1 == 0:
         break
+    sum += in1
     if max < in1:
         max = in1
-print(max)
-
+print("sum: ", sum)
+print("max:", max)
 
