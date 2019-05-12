c69b505e8a647d24ebf8f00de6859aecaa4b03e7
zh9980 <zh9980@163.com>
2019-05-12 22:28:15 +0800

    first commit

--- lesson01/zhanghao/zuoye.py
+++ lesson01/zhanghao/zuoye.py
@@ -5,7 +5,7 @@
 # Version1
 for i in range(1,10):
     for j in range(1,i+1):
-        print("%d * %d = %2d" % (j,i,j*i),end = '  ')
+        print("%d * %d = %2d" % (j,i,j*i),end='  ')
     print('')
 
 # Version2
