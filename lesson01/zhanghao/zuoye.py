647ebd6f2839276b7d2be32146d1f31ed0553bf9
zh9980 <zh9980@163.com>
2019-05-12 22:25:34 +0800

    first commit

--- lesson01/zhanghao/zuoye.py
+++ lesson01/zhanghao/zuoye.py
@@ -1,1 +1,17 @@
-print 123
+'''
+需求：
+打印乘法口诀
+'''
+# Version1
+for i in range(1,10):
+    for j in range(1,i+1):
+        print("%d * %d = %2d" % (j,i,j*i),end = '  ')
+    print('')
+
+# Version2
+i = 1
+while i < 10:
+    for j in range(1,i+1):
+        print("%d * %d = %2d" % (j,i,j*i),end='  ')
+    print('')
+    i = i + 1
