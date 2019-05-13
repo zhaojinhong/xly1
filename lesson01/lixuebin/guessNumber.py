0c0ab690b7171e38a362850099c3d047c1103dea
lixuebin <li.xuebin@hotmail.com>
2019-05-13 17:04:27 +0800

    :cyclone:

---
+++ lesson01/lixuebin/guessNumber.py
@@ -0,0 +1,21 @@
+# 猜六次数字并打印正确与否
+
+import random
+
+num = random.randint(0, 100)
+print(num)
+
+a = 0
+b = 6
+
+while a < b:
+    c = b - (a+1)
+    guessNum = int(input("Please enter the number you guessed: "))
+    if guessNum > num:
+        print("You guessed it was big! You still have {} chances.".format(c))
+    elif guessNum < num:
+        print("You guessed it was small! You still have {} chances.".format(c))
+    else:
+        print("You guessed right. This number is {}".format(num))
+        break
+    a = a + 1
