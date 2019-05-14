ea7a7fb7e479f72d08f4a6b65647d74416d7a884
joe <wenzhiwei@yunzshop.com>
2019-05-14 11:39:55 +0800

    add guess 6 times

--- lesson01/wenzhiwei/guess.py
+++ lesson01/wenzhiwei/guess.py
@@ -8,12 +8,16 @@
 
 
 import random
-number = int(input("input number:"))
-gennumber = random.randint(0, 100)
-print("生成数字: %s" % gennumber)
-if number == gennumber:
-    print("猜对了")
-elif number > gennumber:
-    print("大了")
-else:
-    print("小了")
+
+times = 1
+while times <= 6:
+    number = int(input("times:%s input number:" % times))
+    gennumber = random.randint(0, 100)
+    print("生成数字: %s" % gennumber)
+    if number == gennumber:
+        print("猜对了")
+    elif number > gennumber:
+        print("大了")
+    else:
+        print("小了")
+    times += 1
