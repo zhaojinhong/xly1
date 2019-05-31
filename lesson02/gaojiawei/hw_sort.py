c7ef0e70882f5aae9f231fd8cf1428e0b43330dd
yyuan9 <34882081+yyuan9@users.noreply.github.com>
2019-05-31 14:02:50 +0800

    add yuanyuan/zuoye_v3

--- lesson02/gaojiawei/hw_sort.py
+++
@@ -1,9 +0,0 @@
-#冒泡排序
-my_list = [3,6,1,2,7,9,5]
-for i in range(len(my_list)-1):
-    for j in range(len(my_list)-1):
-        if my_list[j] > my_list[j+1]:
-            tmp = my_list[j]
-            my_list[j] = my_list[j+1]
-            my_list[j+1] = tmp
-print(my_list)
