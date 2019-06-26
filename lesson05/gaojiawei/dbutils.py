42f74ae444c67e74085141d2283472951175d5df
gjwlinux <2970928398@qq.com>
2019-06-26 15:33:18 +0800

    1

--- lesson05/gaojiawei/dbutils.py
+++ lesson05/gaojiawei/dbutils.py
@@ -2,22 +2,15 @@ import pymysql
 
 import configmgt
 
-FILENAME = '51reboot.ini'
+FILENAME = 'dbinfo.ini'
 
 def connnet():
 
-    cfg, ok = configmgt.ReadConfig(FILENAME, 'rebootdb')
+    cfg, ok = configmgt.ReadConfig(FILENAME, 'db')
     if not ok:
         return cfg, False
-    print(cfg)
+
     try:
-        # conn = pymysql.connect(
-        #     host = "10.0.2.15",
-        #     user = "monkey",
-        #     password= "123456",
-        #     database = "ops",
-        #     port = 3306,
-        #     )
         conn = pymysql.connect(
             host=cfg['host'],
             user=cfg['username'],
@@ -104,39 +97,27 @@ def delete(sql):
     finally:
         cur.close()
         conn.close()
+def cleartab(sql):
+    conn = connnet()
+    if not conn:
+        return "conn db fail", False
+    cur = conn.cursor()
+
+    try:
+        cur.execute(sql)
+        print(cur.rowcount)
+        if cur.rowcount == 0:
+            return 'Clear tables fail', False
+
+        conn.commit()
+        return 'Clear tables succ.', True
+    except Exception as e:
+        conn.rollback()
+        return e, False
+    finally:
+        cur.close()
+        conn.close()
 
 
 
-def main():
 
-    # for i in range(10, 30):
-    #     sql = '''insert into users(username,age,tel,email)  values('monkey{}', 12,'132xxx','monkey2@51reboot.com');'''.format(i)
-    #     insertMsg, ok = insert(sql)
-    #     if not ok:
-    #         print(insertMsg)
-
-    # sql = '''delete from users where username = 'monkey10';'''
-    # deleteMsg, ok = delete(sql)
-    # if not ok:
-    #     print(deleteMsg)
-    # sql = '''update users set age = 20 where username = 'monkey11';'''
-    # msg, ok = update(sql)
-    # if not ok:
-    #     print(msg)
-
-    sql = '''select * from users;'''
-    result, ok = select(sql)
-    print(result)
-    if not ok:
-        print(result)
-    else:
-        fields = ['id', 'username', 'age', 'tel', 'email']
-        # for i in result:
-        #     print(dict(zip(fields, i)))
-        retdata = [ dict(zip(fields, i)) for i in result ]
-
-        import json
-        print(json.dumps(retdata, indent=8))
-
-if __name__ == '__main__':
-    main()
