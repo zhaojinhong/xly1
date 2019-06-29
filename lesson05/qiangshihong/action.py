b52dc721e99cc634247ca03081fa01ecfa2939ec
qiangshihong <qianghong000@gmail.com>
2019-06-29 13:31:14 +0800

    1

--- lesson05/qiangshihong/action.py
+++ lesson05/qiangshihong/action.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-from utils import add_user,del_user,get_list,get_pageinfo,find_info,update_info,csv_export,init_info,save_info,load_info
+from utils import add_user,del_user,get_list,get_pageinfo,find_info,update_info
 
 FILENAME = "dic.txt"
 help_info = '''---------------------------------------------
@@ -17,56 +17,53 @@ help_info = '''---------------------------------------------
 
 def logic():
     # 读取文件
-    load_info(FILENAME)
     # 如果输入无效的操作，则反复操作, 否则输入exit退出
     while True:
         try:
-            #初始化判断
-            init_info()
             # 业务逻辑
             info = input("\033[1;35mPlease input your operation: \033[0m")
             # string -> list
-            info_list = info.split()
-            action = info_list[0]
+            input_list = info.split()
+            action = input_list[0]
+            userinfo_list = input_list[1:]
+            #userinfo_string = ' '.join(userinfo_list[1:])
+
             if action == "add":
-                res = add_user(info_list)
+                res = add_user(userinfo_list)
                 print(res)
-                #最后保存变量到文件
-                save_info(FILENAME)
             elif action == "delete" or action == "del":
                 # .remove
-                res = del_user(info_list)
+                res = del_user(userinfo_list)
                 print(res)
-                save_info(FILENAME)
             elif action == "find":
-                res = find_info(info_list)
+                res = find_info(userinfo_list)
                 print(res)
             elif action == "update":
-                res = update_info(info_list)
+                res = update_info(userinfo_list)
                 print(res)
-                save_info(FILENAME)
             elif action == "list":
-                res = get_list(info_list)
-                print(res)
+                get_list()
             elif action == "display":
-                res = get_pageinfo(info_list)
+                res = get_pageinfo(userinfo_list)
                 print(res)
-            elif action == "export":
-                res = csv_export()
-                print(res)
+            # elif action == "export":
+            #     res = csv_export()
+            #     print(res)
             elif action == "help" or action == "h":
                 print(help_info)
             elif action == "exit":
                 sys.exit(0)
             else:
                 print("\033[1;36m输入错误，请输入 help 查看帮助！\033[0m\n")
-        # except IndexError:
-        #     print('\033[1;36m[Errno] list index out of range.\033[0m\n')
-        # except FileNotFoundError:
-        #     print('\033[1;36m[Errno] No such file or directory.\033[0m\n')
-        # except TypeError:
-        #     print('\033[1;36m[Errno] Type Error.\033[0m\n')
-        # except KeyError:
-        #     print('\033[1;36m[Errno] Key Error.\033[0m\n')
+        except IndexError:
+            print('\033[1;36m[Errno] list index out of range.\033[0m\n')
+        except FileNotFoundError:
+            print('\033[1;36m[Errno] No such file or directory.\033[0m\n')
+        except TypeError:
+            print('\033[1;36m[Errno] Type Error.\033[0m\n')
+        except KeyError:
+            print('\033[1;36m[Errno] Key Error.\033[0m\n')
         except Exception as e:
             print(e)
+
+# logic()
