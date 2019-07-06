bd90ff32b9882e458cfcb6b4e0b60cf2b754b208
xoaybc <lxh1031138448@gmail.com>
2019-07-06 09:09:52 +0800

    add v5_class.py

--- lesson06/louxiaohui/utils.py
+++ lesson06/louxiaohui/utils.py
@@ -82,39 +82,6 @@ class DB(object):
 
 
 #db = DB()
-#sql = '''update users set age = 21 where username = 'monkey11';'''
-#result, ok = db.update(sql)
+#sql = '''select * from users where age = 21;'''
+#result, ok = db.select(sql)
 #print (result)
-
-
-def main():
-    #for i in range(10, 30):
-    #    sql = '''insert into users(username,age,tel,email)  values('monkey{}', {},'132xxx','monkey{}@51reboot.com');'''.format(i, i ,i)
-    #    insertMsg, ok = insert(sql)
-    #    if not ok:
-    #        print(insertMsg)
-
-    # sql = '''delete from users where username = 'monkey10';'''
-    # deleteMsg, ok = delete(sql)
-    # if not ok:
-    #     print(deleteMsg)
-    # sql = '''update users set age = 20 where username = 'monkey11';'''
-    # msg, ok = update(sql)
-    # if not ok:
-    #     print(msg)
-
-    sql = '''select * from users limit 2;'''
-    result, ok = select(sql)
-    if not ok:
-        print(result)
-    else:
-        fields = ['id', 'username', 'age', 'tel', 'email']
-        # for i in result:
-        #     print(dict(zip(fields, i)))
-        retdata = [ dict(zip(fields, i)) for i in result ]
-        import json
-        print(json.dumps(retdata, indent=4))
-
-
-#if __name__ == '__main__':
-#    main()
