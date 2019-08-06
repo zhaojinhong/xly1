db38cf5dbc2f9c88e6c88906b5e50543894797b1
aclstack <aclstack@gmail.com>
2019-08-06 17:44:25 +0800

    删除无用项目

--- lesson09/liyongli/devops/devops/settings.py
+++ lesson09/liyongli/devops/devops/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ops.apps.OpsConfig',
-    'apps.users.apps.UsersConfig'
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
