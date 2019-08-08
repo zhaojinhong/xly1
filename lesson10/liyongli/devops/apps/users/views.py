f0c7103285936fdc783ed372b348adff5534ff1c
aclstack <aclstack@gmail.com>
2019-08-09 00:56:33 +0800

    '一用户多组判断'

--- lesson10/liyongli/devops/apps/users/views.py
+++ lesson10/liyongli/devops/apps/users/views.py
@@ -9,26 +9,43 @@ from django.http import JsonResponse, QueryDict
 class UserMangerView(LoginRequiredMixin, View):
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
+
     def get(self, request):
         username = request.GET.get('keyword', '')
         # test = UserProfile.objects.filter(username='liyongli',)
         # filter 结果 < QuerySet[ < UserProfile: liyongli >] >
         # print(test)
-
+        all_user_info = []
+        user_group = []
         if username:
             # 搜索用户
-            user_info = UserProfile.objects.all().values('username', 'name_cn', 'groups__name', 'phone', 'is_active').filter(username=username).first()
+            user_info = UserProfile.objects.all().values('username', 'name_cn', 'groups__name', 'phone', 'is_active'). \
+                filter(username=username).first()
         else:
-            user_info = UserProfile.objects.all().values('username', 'name_cn', 'groups__name', 'phone', 'is_active')
-
-        return render(request, 'user/userlist.html', {'user_info': user_info})
+            all_user = UserProfile.objects.all().values('username')
+            for user in all_user:
+                # 获取全部用户信息
+                user_info = UserProfile.objects.all().values('username', 'name_cn', 'groups__name', 'phone',
+                                                             'is_active').filter(username=user['username'])
+                # 如果长度不为1证明用户属于多个组
+                if len(user_info) != 1:
+                    for users in user_info:
+                        # 获取用户所在的全部组
+                        user_group.append(users['groups__name'])
+                    # 将多用户组数据写入用户信息
+                    user_info[0]['groups__name'] = user_group
+                # 将用户数据加入列表
+                all_user_info.append(user_info[0])
+        return render(request, 'user/userlist.html', {'user_info': all_user_info})
 
 
 class UserUpdateView(LoginRequiredMixin, View):
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
+
     def get(self, request):
-        user_info = UserProfile.objects.all().values('username', 'name_cn', 'phone').filter(username=request.user).first()
+        user_info = UserProfile.objects.all().values('username', 'name_cn', 'phone').filter(
+            username=request.user).first()
         # print(user_info)
         return render(request, 'user/center.html', {'user_info': user_info})
 
@@ -62,8 +79,10 @@ class UserUpdateView(LoginRequiredMixin, View):
 class RoleListView(LoginRequiredMixin, View):
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
+
     def get(self, request):
-        role_info = UserProfile.objects.all().values('groups__name', 'groups__permissions__name').filter(username='liyongli')
+        role_info = UserProfile.objects.all().values('groups__name', 'groups__permissions__name').filter(
+            username='liyongli')
         print(role_info)
         return render(request, 'user/rolelist.html')
 
