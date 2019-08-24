662c7fd7660ffec1f5498f07513afba018331f2e
aclstack <aclstack@gmail.com>
2019-08-25 01:01:30 +0800

    凑活着用版

--- lesson12/liyongli/devops/apps/work_order/templatetags/ordertag.py
+++ lesson12/liyongli/devops/apps/work_order/templatetags/ordertag.py
@@ -5,6 +5,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='orderfile_name')
 def orderfile_name(file_path):
     """
@@ -13,8 +14,9 @@ def orderfile_name(file_path):
     最终需要的格式：aa.txt
     """
     # print(file_path)   # orderfiles/2019/06/aa.txt
-    file_name = str(file_path).split('/')[-1]   # aa.txt
+    file_name = str(file_path).split('/')[-1]  # aa.txt
     return file_name
+
 
 @register.filter(name='userlist_str2')
 def userlist_str2(user_list):
@@ -27,7 +29,6 @@ def userlist_str2(user_list):
         return '%s ...' % ' '.join([user.name_cn for user in user_list[0:2]])
 
 
-
 @register.filter(name='perm_str2')
 def perm_str2(perm_list):
     """
@@ -37,3 +38,29 @@ def perm_str2(perm_list):
         return ' '.join([perm.codename for perm in perm_list])
     else:
         return '%s ...' % ' '.join([perm.codename for perm in perm_list[0:2]])
+
+
+@register.filter(name='get_info')
+def get_info(contents, id):
+    contents_list = []
+    for info in contents:
+        if id == info.order_id.id:
+            # print(info.order_contents)
+            time = str(info.replay_time).split('.')[0]
+
+            contents_list.append("<h4 class='nothh'>" + info.write_user + ": " + "</h4>" + info.order_contents +
+                                 "<br> <br>" + time)
+    return list(reversed(contents_list))
+
+
+@register.filter(name='cut_info')
+def cut_info(contents):
+    note = ''
+    for i in contents:
+        note += '<pre>' + i + '</pre>'
+    return note
+
+
+@register.filter(name='get_data')
+def get_data(data):
+    return data['contents']
